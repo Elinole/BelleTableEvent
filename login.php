239d1535bf331e67e976d4154863a8bee0f75256
Elias <elinole94@gmail.com>
2018-01-28 21:40:15 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,94 @@
+<?php
+	include_once "header.php";
+?>
+
+<body class="signup-page">
+	<?php
+	include_once "navbar.php";
+	?>
+
+    <div class="wrapper">
+		<div class="header header-filter" style="background-image: url('assets/img/haley-powers-370161.jpg'); background-size: cover; background-position: top center;">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
+						<div class="card card-signup">
+							<form class="form" method="" action="">
+								<div class="header header-warning text-center">
+									<h4>Se connecter</h4>
+									<div class="social-line">
+										<a href="#pablo" class="btn btn-simple btn-just-icon">
+											<i class="fa fa-facebook-square"></i>
+										</a>
+										<a href="#pablo" class="btn btn-simple btn-just-icon">
+											<i class="fa fa-twitter"></i>
+										</a>
+										<a href="#pablo" class="btn btn-simple btn-just-icon">
+											<i class="fa fa-google-plus"></i>
+										</a>
+									</div>
+								</div>
+								<a href="passwordforgot.php"><p class="text-divider">Mot de passe oublié ?</p></a>
+								<div class="content">
+
+									<div class="input-group">
+										<span class="input-group-addon">
+											<i class="material-icons">email</i>
+										</span>
+										<input type="text" class="form-control" placeholder="Email...">
+									</div>
+
+									<div class="input-group">
+										<span class="input-group-addon">
+											<i class="material-icons">lock_outline</i>
+										</span>
+										<input type="password" placeholder="Mot de passe..." class="form-control" />
+									</div>
+
+									<!-- If you want to add a checkbox to this form, uncomment this code
+
+									<div class="checkbox">
+										<label>
+											<input type="checkbox" name="optionsCheckboxes" checked>
+											Subscribe to newsletter
+										</label>
+									</div> -->
+								</div>
+								<div class="footer text-center">
+									<input type="submit" class="btn btn-simple btn-warning btn-lg" value="Connexion">
+								</div>
+							</form>
+
+							<div class="footer text-center">
+								<a href="register.php" class="btn btn-simple btn-warning btn-lg">Pas encore de compte ?</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<?php
+				include_once "footer.php";
+			?>
+
+		</div>
+
+    </div>
+
+
+</body>
+	<!--   Core JS Files   -->
+	<script src="assets/js/jquery.min.js" type="text/javascript"></script>
+	<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="assets/js/material.min.js"></script>
+
+	<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+	<script src="assets/js/nouislider.min.js" type="text/javascript"></script>
+
+	<!--  Plugin for the Datepicker, full documentation here: http://www.eyecon.ro/bootstrap-datepicker/ -->
+	<script src="assets/js/bootstrap-datepicker.js" type="text/javascript"></script>
+
+	<!-- Control Center for Material Kit: activating the ripples, parallax effects, scripts from the example pages etc -->
+	<script src="assets/js/material-kit.js" type="text/javascript"></script>
+
+</html>
